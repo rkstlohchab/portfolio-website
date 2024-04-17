adaec115bf6661d7bf11739e2f7ee910534da6e7
Rakshit Kumar <rkstlohchab@gmail.com>
2024-04-17 21:07:36 +0530

    fixed cursor

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -9,6 +9,7 @@ const Hero = () => {
         controls={false}
         muted
         loop
+        playsInline
         className="rotate-180 absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover "
       >
         <source src="/blackhole.webm" type="video/webm" />
